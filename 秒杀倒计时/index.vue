df29b2571615c4277b6ee82aa564a0ec83470688
huge <1904681622@qq.com>
2019-07-08 18:42:28 +0800

    秒杀倒计时/index.vue

---
+++ 秒杀倒计时/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Spike :start-time="startTime" :end-time="endTime" />
+  </div>
+</template>
+<script>
+import moment from "moment";
+import Spike from "./Spike";
+export default {
+  components: {
+    Spike
+  },
+  data() {
+    return {
+      startTime: moment("2019-07-08 18:55:00"),
+      endTime: moment("2019-07-08 19:40:00")
+    };
+  }
+};
+</script>
